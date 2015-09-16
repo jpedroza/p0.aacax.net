3fa64c4862e77afee4f02ba4979ae7cfe06cc786
John Pedroza <Mr.John.Pedroza@gmail.com>
2015-09-16 15:07:36 -0600

    adding my first file

---
+++ hello.php
@@ -0,0 +1,13 @@
+<!doctype  html>
+<html>
+  <head>
+    <title>Hello World!</title>
+    <meta charset='utf-8'>
+  </head>
+  <body>
+    <?php
+      echo 'dude wnat is up there ';
+      echo $_SERVER['SERVER_NAME'];
+    ?>
+  </body>
+</html>
